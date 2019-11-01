2e62fa8847607467102976282e67993b1863eb24
daager <nurlan@medsovet.info>
2019-11-01 23:51:09 +0300

    Окрашивание фона бумаги в красный или зеленый в зависимости от доходности.

--- extension/src/inject/tinkoff_site.js
+++ extension/src/inject/tinkoff_site.js
@@ -1,4 +1,6 @@
 async function real_revenue() {
+    result=0
+
     function getCookie(name) {
         let matches = document.cookie.match(new RegExp(
             "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -46,35 +48,61 @@ async function real_revenue() {
 
 // current_ticker=Object.keys(data.investTrade.symbolUserInfo)[0];
     regex = /\(([A-Z\.]+)\):/m
-    current_ticker = regex.exec(document.querySelector("meta[property='og:title']").content)[1]
-    history_result = await getHistory(current_ticker);
-    info = await getInfo(current_ticker)
+    ticker_regexp=regex.exec(document.querySelector("meta[property='og:title']").content)
+    if(ticker_regexp!==null) {
+        current_ticker = ticker_regexp[1]
+        history_result = await getHistory(current_ticker);
+        info = await getInfo(current_ticker)
 
-    history_result = history_result.payload.items.reduce(function (sum, item) {
-        if (item.status == 'done') {
-            //console.log(item.payment,(item.commission||0))
-            return sum + item.payment + (item.commission || 0);
+        history_result = history_result.payload.items.reduce(function (sum, item) {
+            if (item.status == 'done') {
+                //console.log(item.payment,(item.commission||0))
+                return sum + item.payment + (item.commission || 0);
+            }
+            return sum;
+        }, 0)
+
+        result = Math.round(history_result);
+
+        if (info.payload.hasEvents) {
+            result = info.payload.positionTinkoff.currentAmount.value - Math.abs(result)
         }
-        return sum;
-    }, 0)
+        result = parseFloat(result).toFixed(2);
 
-    result = Math.round(history_result)
+        html = '<div><b class="real_revenue">Реальный заработок: ' + result + '</b></div>';
 
-    if (info.payload.hasEvents) {
-        result = info.payload.positionTinkoff.currentAmount.value - Math.abs(result)
+
+        b_real = document.querySelector("b.real_revenue")
+        if (b_real) {
+            b_real.outerHTML = html
+        } else {
+            document.querySelector("h1[data-qa-file^=SecurityHeader]").insertAdjacentHTML('afterend', html)
+        }
     }
-    result = parseFloat(result).toFixed(2);
 
-    html = '<b class="real_revenue">Реальный заработок: ' + result + '</b>';
-    b_real = document.querySelector("b.real_revenue")
-    if (b_real) {
-        b_real.outerHTML = html
+    if(result<0){
+        document.body.classList.remove('good-revenue');
+        document.body.classList.add('bad-revenue');
+    } else if(result>0) {
+        document.body.classList.add('good-revenue');
+        document.body.classList.remove('bad-revenue');
     } else {
-        document.querySelector("h1[data-qa-file=SecurityHeaderDesktopPure]").insertAdjacentHTML('afterend', html)
+        document.body.classList.remove('good-revenue');
+        document.body.classList.remove('bad-revenue');
     }
 }
 
 if (window.location.host == 'www.tinkoff.ru') {
+    style_arr = [
+        '.bad-revenue{background-color: rgba(255, 0, 0, 0.05);}',
+        '.good-revenue{background-color: rgba(0, 255, 0, 0.05);}'
+    ];
+
+    style_arr.forEach(function (style) {
+        document.body.insertAdjacentHTML("afterbegin", "<style>" + style + "</style>")
+    })
+
+
     real_revenue();
     setInterval(async function () {
         await real_revenue()
