da81b793bbec7d7ed4eef2ccefb7ae125ab4e4ab
daager <nurlan@medsovet.info>
2019-11-02 02:51:03 +0300

    fix если бумага торговалась, но сейчас ничего не осталось

--- extension/src/inject/tinkoff_site.js
+++ extension/src/inject/tinkoff_site.js
@@ -1,12 +1,12 @@
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 async function real_revenue() {
     result = 0
-
-    function getCookie(name) {
-        let matches = document.cookie.match(new RegExp(
-            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-        ));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
 
     async function getHistory(ticker) {
         let data = {
@@ -46,17 +46,17 @@ async function real_revenue() {
         return await response.json();
     }
 
-    function needEarn(revenue,position_tinkoff){
-        count=info.payload.positionTinkoff.currentBalance
-        avg_price=parseFloat(info.payload.positionTinkoff.averagePositionPrice.value)
-        currency=info.payload.positionTinkoff.averagePositionPrice.currency
+    function needEarn(revenue, position_tinkoff) {
+        count = info.payload.positionTinkoff.currentBalance
+        avg_price = parseFloat(info.payload.positionTinkoff.averagePositionPrice.value)
+        currency = info.payload.positionTinkoff.averagePositionPrice.currency
 
-        revenue=Math.abs(revenue);
-        need_earn_money=parseFloat(parseFloat(revenue/count).toFixed(2));
-        percent=avg_price/100;
-        need_earn_percent=parseFloat(need_earn_money/percent).toFixed(2);
+        revenue = Math.abs(revenue);
+        need_earn_money = parseFloat(parseFloat(revenue / count).toFixed(2));
+        percent = avg_price / 100;
+        need_earn_percent = parseFloat(need_earn_money / percent).toFixed(2);
 
-        return (avg_price+need_earn_money)+"("+"+"+need_earn_money+currency+"/"+need_earn_percent+"%)"
+        return (avg_price + need_earn_money) + "(" + "+" + need_earn_money + currency + "/" + need_earn_percent + "%)"
     }
 
 // current_ticker=Object.keys(data.investTrade.symbolUserInfo)[0];
@@ -79,19 +79,19 @@ async function real_revenue() {
             }, 0)
 
             result = Math.round(history_result);
-            if(info.payload.positionTinkoff) {
+            if (info.payload.positionTinkoff) {
                 result = info.payload.positionTinkoff.currentAmount.value - Math.abs(result)
             }
             result = parseFloat(result).toFixed(2);
 
 
-            html="<div class='tinvest-block'>";
+            html = "<div class='tinvest-block'>";
             html += '<div><b class="real_revenue">Реальный заработок: ' + result + '</b></div>';
-            if(result<0 && info.payload.positionTinkoff){
-                need_earn=needEarn(result,info.payload.positionTinkoff)
-                html+='<div><b class="need_earn">Выход в ноль при: ' + need_earn + '</b></div>';
+            if (result < 0 && info.payload.positionTinkoff) {
+                need_earn = needEarn(result, info.payload.positionTinkoff)
+                html += '<div><b class="need_earn">Выход в ноль при: ' + need_earn + '</b></div>';
             }
-            html+="</div>"
+            html += "</div>"
 
             tinvest_block = document.querySelector("div.tinvest-block")
             if (tinvest_block) {
@@ -115,6 +115,69 @@ async function real_revenue() {
     }
 }
 
+function exportToCsv() {
+    btn_tpl = document.querySelector('a[href="/invest/broker_account/about/"]')
+    if (!btn_tpl || document.querySelector("span.tinvest-export")) {
+        return false
+    }
+    btn = btn_tpl.parentElement.cloneNode(true);
+    btn.querySelector('a').href = "#"
+    btn.querySelector('a span').classList.add('tinvest-export')
+    btn.querySelector('span span').textContent = "Экспорт в CSV"
+    btn_tpl.parentElement.insertAdjacentHTML('afterend', btn.outerHTML)
+
+    document.onclick = async function (e) {
+        var e = e || window.event, el = e.target || el.srcElement;
+
+        if (el.classList.contains('tinvest-export') || el.parentElement.classList.contains('tinvest-export')) {
+            let data = {
+                "stocksSort": "ByName",
+                "stocksSortOrder": "Asc",
+                "bondsSort": "ByName",
+                "bondsSortOrder": "Asc",
+                "etfsSort": "ByName",
+                "etfsSortOrder": "Asc",
+                "brokerAccountType": "Tinkoff",
+                "currency": "RUB"
+            };
+
+            let s_id = getCookie('psid')
+            let response = await fetch(' https://api.tinkoff.ru/trading/portfolio/purchased_securities?tinvest&sessionId=' + s_id, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(data)
+            });
+
+            result = (await response.json()).payload.data;
+            csv_arr = [['тикер', 'количество', 'валюта', 'ср.цена', 'цена', 'стоимость', 'доходность', 'доходность %']];
+            result.forEach(function (item) {
+                if (item.securityType == 'Stock') {
+                    csv_arr.push([item.ticker, item.currentBalance, item.averagePositionPrice.currency, item.averagePositionPrice.value, item.currentPrice.value, item.currentAmount.value, item.expectedYield.value, item.expectedYieldRelative])
+                }
+            })
+
+            let csvContent_header = "data:text/csv;charset=utf-8,%EF%BB%BF";
+
+            csvContent = ''
+            csv_arr.forEach(function (rowArray) {
+                let row = rowArray.join(";").replace(/\./g, ',');
+                csvContent += row + "\r\n";
+            });
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", csvContent_header + encodedUri);
+            date = new Date();
+            date_format = date.getDate() + "." + date.getMonth() + "." + date.getFullYear()
+            link.setAttribute("download", "stocks_" + date_format + ".csv");
+            document.body.appendChild(link);
+
+            link.click();
+        }
+    };
+}
+
 if (window.location.host == 'www.tinkoff.ru') {
     style_arr = [
         '.bad-revenue{background-color: rgba(255, 0, 0, 0.05);}',
@@ -125,11 +188,12 @@ if (window.location.host == 'www.tinkoff.ru') {
         document.body.insertAdjacentHTML("afterbegin", "<style>" + style + "</style>")
     })
 
-
+    exportToCsv();
     real_revenue();
     setInterval(async function () {
-        await real_revenue()
-    }, 2500);
+        await real_revenue();
+        exportToCsv();
+    }, 2000);
 } else {
     console.log('not tinkoff')
 }
